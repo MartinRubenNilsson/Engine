a69f0b6f76095a56ed6db0198fb733be7bc4d142
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2023-02-16 23:43:45 +0100

    Small shader refactoring

--- PsPbr.hlsl
+++ PsPbr.hlsl
@@ -4,17 +4,16 @@
 float4 main(float4 aPixelPosition : SV_Position) : SV_TARGET
 {
     uint2 dim;
-    GBufferWorldPosition.GetDimensions(dim.x, dim.y);
-    
+    GBufferPixelNormal.GetDimensions(dim.x, dim.y);
     const float2 uv = aPixelPosition.xy / dim;
     
-    const float4 worldPosition  = GBufferWorldPosition.Sample(DefaultSampler, uv);
-    const float4 pixelNormal    = GBufferPixelNormal.Sample(DefaultSampler, uv);
-    const float4 albedo         = GBufferAlbedo.Sample(DefaultSampler, uv);
-    const float4 metalRoughAo   = GBufferMetalRoughAo.Sample(DefaultSampler, uv);
-    
+    const float4 pixelNormal = GBufferPixelNormal.Sample(DefaultSampler, uv);
     if (!any(pixelNormal))
         discard;
+    
+    const float4 worldPosition  = GBufferWorldPosition.Sample(DefaultSampler, uv);
+    const float4 albedo         = GBufferAlbedo.Sample(DefaultSampler, uv);
+    const float4 metalRoughAo   = GBufferMetalRoughAo.Sample(DefaultSampler, uv);
     
     const float3 L = normalize(float3(-1.f, 1.f, 0.f));
     const float3 V = normalize(CameraPosition.xyz - worldPosition.xyz);
