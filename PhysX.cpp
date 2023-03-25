f9ee20fb7869d171366d34596eaf3e403a2648b8
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2023-03-25 12:04:48 +0100

    Work on physx singleton

--- PhysX.cpp
+++ PhysX.cpp
@@ -1,17 +1,38 @@
 #include "pch.h"
 #include "PhysX.h"
 
+#define PVD_HOST "127.0.0.1"
+
 PhysX::PhysX()
 {
-	myFoundation.reset(PxCreateFoundation(
-		PX_PHYSICS_VERSION,
-		myAllocator,
-		myErrorCallback
-	));
+	myFoundation.reset(PxCreateFoundation(PX_PHYSICS_VERSION, myAllocator, myErrorCallback));
 	if (!myFoundation)
 		return;
 
+#ifdef _DEBUG
+	myPvdTransport.reset(PxDefaultPvdSocketTransportCreate(PVD_HOST, 5425, 10));
+	if (!myPvdTransport)
+		return;
+
+	myPvd.reset(PxCreatePvd(*myFoundation));
+	if (!myPvd)
+		return;
+#endif
+
 	mySucceeded = true;
+}
+
+bool PhysX::ConnectPvd()
+{
+	if (myPvd && myPvdTransport)
+		return myPvd->connect(*myPvdTransport, PxPvdInstrumentationFlag::eALL);
+	return false;
+}
+
+void PhysX::DisconnectPvd()
+{
+	if (myPvd)
+		myPvd->disconnect();
 }
 
 PhysX::operator bool() const
--- PhysX.h
+++ PhysX.h
@@ -5,12 +5,17 @@ class PhysX
 public:
 	PhysX();
 
+	bool ConnectPvd();
+	void DisconnectPvd();
+
 	explicit operator bool() const;
 
 private:
 	PxDefaultAllocator myAllocator{};
 	PxDefaultErrorCallback myErrorCallback{};
 	PxPtr<PxFoundation> myFoundation{};
+	PxPtr<PxPvdTransport> myPvdTransport{};
+	PxPtr<PxPvd> myPvd{};
 	bool mySucceeded{ false };
 };
 
